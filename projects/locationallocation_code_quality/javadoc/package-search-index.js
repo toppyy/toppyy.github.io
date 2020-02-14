0d4b7351fbcef9f1c8f3bfaddb9eb5360384e33b
toppyy <topias.pyykkonen@helsinki.fi>
2020-02-14 21:44:57 +0200

    added ArrayList class

---
+++ projects/locationallocation_code_quality/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"locationallocation"},{"l":"locationallocation.Domain"},{"l":"locationallocation.Qualitytest"},{"l":"locationallocation.Utils"}]
