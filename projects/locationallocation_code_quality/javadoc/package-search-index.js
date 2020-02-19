1f4c318049d2429e18e5d3611ba45a99c86ee759
toppyy <topias.pyykkonen@helsinki.fi>
2020-02-19 20:49:20 +0200

    .Domain included in javadocs

--- projects/locationallocation_code_quality/javadoc/package-search-index.js
+++ projects/locationallocation_code_quality/javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"locationallocation"},{"l":"locationallocation.Domain"},{"l":"locationallocation.Qualitytest"},{"l":"locationallocation.Utils"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"locationallocation"},{"l":"locationallocation.Domain"},{"l":"locationallocation.Qualitytest"},{"l":"locationallocation.UI"},{"l":"locationallocation.Utils"}]
